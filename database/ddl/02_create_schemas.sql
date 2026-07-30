d8e985c0556162c2f74e64f9707c2dbeafc58820
Renato Novaes <renatovenancionovaes26@gmail.com>
2026-07-30 16:50:12 -0300

    Add schema creation script

---
+++ database/ddl/02_create_schemas.sql
@@ -0,0 +1,19 @@
+/*
+==========================================
+Wealth Management Analytics Dashboard
+On Time Analytics Services
+
+Script 02
+Create Schemas
+==========================================
+*/
+
+CREATE SCHEMA IF NOT EXISTS staging;
+
+CREATE SCHEMA IF NOT EXISTS dw;
+
+CREATE SCHEMA IF NOT EXISTS audit;
+
+CREATE SCHEMA IF NOT EXISTS etl;
+
+CREATE SCHEMA IF NOT EXISTS mart;
